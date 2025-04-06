16bdde2c91661d36c3f70fc2e2967606af62b096
vi66ko <vi66ko@mail.bg>
2025-04-06 19:16:17 +0100

    Chatper 2.1 Overview

--- src/main/java/org/openjfx/_2_scene_graph/_2_1_Overview.java
+++ src/main/java/org/openjfx/_2_scene_graph/_2_1_Overview.java
@@ -1,8 +1,22 @@
 package org.openjfx._2_scene_graph;
 
-/**
- * 2.1 Overview
- */
-public class _2_1_Overview {
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
 
+// Layout
+import javafx.scene.layout.StackPane;
+
+// Components
+import javafx.scene.text.Text;
+
+public class _2_1_Overview {
+    public _2_1_Overview(Stage stage) {
+        stage.setScene(new Scene(this.createContent(), 300, 300));
+        stage.show();
+    }
+
+    private Parent createContent() {
+        return new StackPane(new Text("Namaste World"));
+    }
 }
